7018b6bfeb28854ac3ac67a1851ba48e9554dec4
LAPTOP-QO24TO2E\10539 <1053925943@qq.com>
2023-09-07 15:31:04 +0800

    PEDATA MAX

--- PEDATA MAX/PEDATA.py
+++ PEDATA MAX/PEDATA.py
@@ -3,7 +3,7 @@ import execjs
 import requests
 
 news_est_url = "https://max.pedata.cn/api/q4x/newsflash/list"
-login_token = "424a86714113e18879e2ddd853473aa82fcb75f59df3512c8692bc561dca3bc4"
+login_token = "424a86714113e18879e2ddd853473aa8e922e51c9fa50f9a1c24e3d0f63953e0"
 headers = {
     'Accept': 'application/json, text/plain, */*',
     'Accept-Language': 'zh-CN,zh;q=0.9',
@@ -31,7 +31,7 @@ headers = {
 def get_decrypted_data(encrypted_data, exor):
     with open('PEDATA.js', 'r', encoding='utf-8') as f:
         pedata_js = f.read()
-    decrypted_data = execjs.compile(pedata_js).call('q', encrypted_data, exor, login_token)
+    decrypted_data = execjs.compile(pedata_js).call('getDecryptedData', encrypted_data, exor, login_token)
     return decrypted_data
 
 
